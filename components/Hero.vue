a5c5e810e08ae354c39f690b20724570b613195f
Sam Xie <sxie@zendesk.com>
2020-04-29 22:30:36 +1000

    set up with hero

---
+++ components/Hero.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="background">
+    <div class="container">
+      <div class="text-container">
+        <h1>Sam Xie</h1>
+        <h2>Software engineer at <a href="https://www.zendesk.com/about/" target="_blank">Zendesk.</a></h2>
+        <h2>Indie software dev launching <a href="/#products">one product</a> at a time.</h2>
+        <div class="social-links">
+          <h2><a href="https://twitter.com/samxstudio" target="_blank">Twitter</a></h2>
+          <h2><a href="https://twitter.com/samxstudio" target="_blank">Medium</a></h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.background {
+  background: url("https://res.cloudinary.com/xielabs/image/upload/v1588150730/spheres.jpg");
+  background-size: cover;
+  background-position: 50%;
+  height: 800px;
+  width: 100%;
+  min-width: 400px;
+  max-width: 1600px;
+  margin: auto;
+}
+
+.container {
+  max-width: 980px;
+  margin: auto;
+  padding-top: 180px;
+}
+
+.text-container {
+  width: 380px;
+  margin: 10px;
+}
+
+.social-links {
+  margin: 46px 0px;
+  display: flex;
+}
+
+.social-links h2 {
+  margin-right: 32px
+}
+
+h1, h2 {
+  font-weight: bold;
+}
+
+h1 {
+  font-size: 96px;
+  margin-bottom: 32px;
+}
+
+h2 {
+  font-size: 24px;
+  margin: 16px 0px;
+}
+
+a {
+  color: #3B70A2;
+  text-decoration: none;
+}
+
+@media screen and (max-width: 980px) {
+  .container {
+    padding-top: 120px;
+  }
+  .background {
+    background-position: 54% 0px;
+    background-size: inherit;
+  }
+  h1 {
+    font-size: 72px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <hero/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hero from '~/components/Hero.vue'
+
+export default {
+  components: {
+    Hero
+  }
+}
+</script>
+
+<style>
+
+</style>
